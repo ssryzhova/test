a9126109126f19c16de6eec77ccb817c04c33a91
ssryzhova <sofia.ryzhova1710@rambler.ru>
2026-05-27 21:45:24 +0300

    test

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fs)
+
+	log.Println("Server started on :8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
